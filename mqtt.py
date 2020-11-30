adcffdce22fab30f46a81df0701384782fcc05ae
Scotty <reddirtplace@protonmail.com>
2020-11-29 22:30:03 -0600

    Removed code no longer being used

--- mqtt.py
+++ mqtt.py
@@ -14,23 +14,3 @@ def mqtt_client(topic, msg):
 
     client.publish(topic, msg)
     client.disconnect()
-
-
-# class MqttConnect:
-
-#     mqtt_server = 'test.mosquitto.org'
-#     client_id = ubinascii.hexlify(machine.unique_id())
-
-#     def __init__(self, topic, msg, port=1883):
-
-#         self.topic = topic
-#         self.msg = msg
-#         self.port = port
-
-#     def client(self):
-
-#         client = MQTTClient(self.client_id, self.mqtt_server, self.port)
-#         client.connect()
-
-#         client.publish(self.topic, self.msg)
-#         client.disconnect()
