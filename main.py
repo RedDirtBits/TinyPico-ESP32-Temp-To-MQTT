8d17845bef7780d56b943a27e8ce32e09eb7040a
Scotty <reddirtplace@protonmail.com>
2020-12-02 01:07:20 -0600

    Condensed sensor reads and removed sleep timers

--- main.py
+++ main.py
@@ -10,44 +10,28 @@ from mqtt import mqtt_client
 # due to the sleep cycles.  As well, no where is there any Error handling.  That will
 # have to be added, especially on the network connectivity side.
 
+
 # Connect to WiFi
 NetworkConnection().wifi_connect()
 
 # The device seemed to be able to process faster than the connection
 # being fully established and stable, so sleep for a few secs.
 
-utime.sleep(3)
-
-# Read from the sensor and store results
-
-dht11_temp_c = sensors.read_dht11()[0]
-dht11_temp_f = sensors.read_dht11()[1]
-dht11_humidity = sensors.read_dht11()[2]
-
-bmp180_temp = (sensors.read_bmp180()[0] * 1.8) + 32
-bmp180_pressure = sensors.read_bmp180()[1] * 0.00030
-bmp180_altitude = sensors.read_bmp180()[2] * 3.2808
-
-lux = sensors.read_bh1750()
-
-
-# Sleep again just to be sure the sensor read completes
-
-utime.sleep(3)
+utime.sleep(1)
 
 sensor_data = {
     'DHT11': {
-        'Current Temperature (C)': str(float(dht11_temp_c)),
-        'Current Temperature (F)': str(float(dht11_temp_f)),
-        'Current Humidity (%)': str(float(dht11_humidity))
+        'Current Temperature (C)': round(float(sensors.read_dht11()[0]), 2),
+        'Current Temperature (F)': round(float(sensors.read_dht11()[1]), 2),
+        'Current Humidity (%)': round(float(sensors.read_dht11()[2]), 2)
     },
     'BMP180': {
-        'Current Temperature (F)': bmp180_temp,
-        'Current Atmospheric Pressure (inHg)': bmp180_pressure,
-        'Current Altitude (Ft)': bmp180_altitude
+        'Current Temperature (F)': round(((sensors.read_bmp180()[0] * 1.8) + 32), 2),
+        'Current Atmospheric Pressure (inHg)': round(sensors.read_bmp180()[1] * 0.00030, 2),
+        'Current Altitude (Ft)': round(sensors.read_bmp180()[2] * 3.2808, 2)
     },
     'BH1750': {
-        'Current Light Level (Lux)': lux
+        'Current Light Level (Lux)': round(sensors.read_bh1750(), 2)
     }
 }
 
@@ -63,11 +47,11 @@ NetworkConnection().wifi_disconnect()
 # Start a simple sleep cycle to provide time to reset or upload/update
 # code while in development
 
-# utime.sleep(10)
+utime.sleep(10)
 
 # The deepsleep time is in microseconds.  Therefore, to calculate deepsleep time,
 # multiple the time in seconds you want to wait for the next cycle by 1000.
 # For example, 15 minutes would be 900 seconds.  A 15 minute deepsleep would
 # be 900 x 1000 or 90000.
 
-# machine.deepsleep(300000)
+machine.deepsleep(300000)
